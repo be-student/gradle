5dec1218cc40e2b48b84f1c7d41128e2d0205228
Be-student <presentsong@naver.com>
2022-12-26 15:35:17 +0900

    single ci test

--- multi/settings.gradle.kts
+++ multi/settings.gradle.kts
@@ -9,3 +9,17 @@
 
 rootProject.name = "multi"
 include("app", "list", "utilities")
+
+plugins {
+    id("com.gradle.enterprise") version("3.9")
+}
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
